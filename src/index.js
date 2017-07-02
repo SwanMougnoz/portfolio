52e3c8ec0e9bdd631a0ef054a1d34e8f0faef6c3
swan <mougnoz.swan@gmail.com>
2017-07-02 13:02:09 +0200

    Add semantic ui to the project dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App/App';
+
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
